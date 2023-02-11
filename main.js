eafbcdbd62a3c01a2a29383e7161f14c9a8e4665
RaLiotti <rafa.liotti@gmail.com>
2023-02-10 22:32:00 -0300

    criação de nova branch

--- main.js
+++ main.js
@@ -11,12 +11,12 @@ button.addEventListener("click", function() {
 fetch(url)
     .then((response) => response.json())
     .then(jsonBody => jsonBody.results)
-    .then((pokemons) => pokemons.map((pokeApi.getPokemonDetail))) 
-    .then((detailRequests) => Promise.all(detailRequests))
-    .then((pokemonsDetails) => pokemonsDetails)
-
-     
-    
+    .then((pokemons) => pokemons.map((getPokemonDetail)))
     .catch(error => {console.error(error)})
-    console.log(pokemonsDetails)
 })
+
+getPokemonDetail = (pokemon) => {
+  return fetch(pokemon.url)
+      .then((response) => response.json())
+      .then((covertPokeApiDetailToPokemon => console.log(covertPokeApiDetailToPokemon)))
+};
