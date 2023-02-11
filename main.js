38686b6a9668bc8d4cd14bbd65b5b8822fb26488
Leonardo Schaffer <leoschaffer@hotmail.com>
2023-02-11 11:30:08 -0300

    atualização

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
-class Pokemon {
-  number ;
-  name ;
-  type;
-  types = [];
-  photo;
-  evolutions = []; 
-  weight;
-  height;
-  stats = [];
-  stat =[];
-}
+
 
 
 
@@ -22,7 +11,7 @@ button.addEventListener("click", function() {
   const url = `https://pokeapi.co/api/v2/pokemon/${searchTerm}`;
   console.log(url);
 
-  
+ 
 fetch(url)
     .then((response) => response.json())
     .then(jsonBody => {
@@ -43,18 +32,47 @@ fetch(url)
 
       pokemon.types = types
       pokemon.type = type
+     
       pokemon.photo = jsonBody.sprites.other.dream_world.front_default
       pokemon.weight = jsonBody.weight
       pokemon.height = jsonBody.height
 
       console.log(pokemon)
 
+      const newDiv = document.createElement('div');
+      newDiv.innerHTML = 
+      `
+      <div id="pokemonMod" class="pokemon">
+            <div class="card" style="width: 18rem;">
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+                <div class="card-body">
+                  <h5 class="card-title">${pokemon.name}</h5>
+                  <h2 class="numero">#${pokemon.number}</h2>
+                    <ol class="types">
+                      ${pokemon.types.map((type) => `<li class="type${type}">${type}</li>`).join('')}
+                    </ol> 
+                    <p class="card-text">peso${pokemon.weight}</p>
+                    <p class="card-text">altura${pokemon.height}</p>
+                </div>
+                <div class="status">
+                  <div class="ata">
+                   ${pokemon.stats.map((stat) => `<p>${stat}</p>`).join('')}
+                  </div>
+                  <div class="pontos">
+                    ${pokemon.stat.map((stat) => `<p>${stat}</p>`).join('')}
+                  </div>                
+                </div>
+
+            </div>    
+      </div>       
+
+
+      `;
+      document.body.appendChild(newDiv);
+
   
     })
     .catch(error => {console.error(error)})
     
-    
-
-
     
 })
